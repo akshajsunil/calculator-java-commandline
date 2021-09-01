58abf1d1a3c2918c8290f1d50497160cacc3a18a
Akshaj Sunil <akshajxf@gmail.com>
2021-09-01 11:14:27 +0530

    divide by zero error added

--- src/cal.java
+++ src/cal.java
@@ -7,6 +7,7 @@ public class cal {
         Scanner input = new Scanner(System.in);
         a = input.nextDouble();
         b= input.nextDouble();
+
         op = input.next().charAt(0);
         switch (op)
         {
@@ -16,7 +17,13 @@ public class cal {
             break;
             case '*' : System.out.println(a*b);
             break;
-            case '/' :System.out.println(a/b);
+            case '/' :
+                if(b==0)
+                {
+                    System.out.println("Cannot divide by zero!!");
+                    break;
+                }
+                System.out.println(a/b);
             break;
             default: System.out.println("Invalid operator");
 
